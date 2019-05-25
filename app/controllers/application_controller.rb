7dc9c4a42953b44c00a73caa2ec99efb5b9fdaa2
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2019-05-25 16:08:11 +0900

    initial comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  config.generators do |g|
-    g.stylesheets false
-    g.javascripts false
-    g.helper false
-    g.test_framework false
-  end
 end
