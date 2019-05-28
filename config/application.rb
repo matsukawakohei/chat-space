042b775d6371104c18c3f64ad421200f08f9f576
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2019-05-28 19:04:11 +0900

    make index view

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,11 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
+      g.helper     false
+      g.test_framework false
+      g.javascripts false
+
+      
     end
   end
 end
