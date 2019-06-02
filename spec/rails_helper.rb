85cec4434d507e9f844bff1c51126f3f9faeee19
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2019-06-02 13:26:37 +0900

    devise test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,4 +62,10 @@ RSpec.configure do |config|
     config.include FactoryBot::Syntax::Methods
     #〜省略〜
   end
+  RSpec.configure do |config|
+    Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+    config.include Devise::Test::ControllerHelpers, type: :controller
+    config.include ControllerMacros, type: :controller
+    #〜省略〜
+  end
 end
