482788076e46887dbfc21d973d036c9fdd22ad6d
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2019-06-12 18:33:34 +0900

    take12

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,25 +4,9 @@ lock '~> 3.11.0'
 set :application, 'chat-space'
 set :repo_url, 'git@github.com:matsukawakohei/chat-space.git'
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-set :rbenv_type, :user
-set :rbenv_ruby, '<2.5.1>'
 
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/koheimatsukawa.pem']
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
-set :composer_install_flags, '--no-dev --prefer-dist --no-interaction --optimize-autoloader'
-
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -53,7 +37,28 @@ end
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
+set :keep_releases, 5
 # set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '<2.5.1>'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/koheimatsukawa.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+set :composer_install_flags, '--no-dev --prefer-dist --no-interaction --optimize-autoloader'
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+server '<52.197.214.81>', user: 'ec2-user', roles: %w{app db web}
 
 
 # role-based syntax
@@ -59,11 +59,3 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-
-server '<52.197.214.81>', user: 'ec2-user', roles: %w{app db web}
-set :ssh_options, {
-  port: 80,
-  keys: [File.expand_path('koheimatsukawa.pem')],
-  forward_agent: true,
-  auth_methods: %w(publickey)
-}
